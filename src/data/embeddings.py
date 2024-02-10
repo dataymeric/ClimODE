fix: NaN Loss due to an error in embedding calculation
(cherry picked from commit dbce7cf2972a3ba6f5912d1b631e5059cc1eea02)

src/data/embeddings.py
@@ -23,7 +23,9 @@ def get_day_and_season_embeddings(day_of_year_ratio, hour_of_day):
         - Column 3: cos seasonal embedding
     """
     # étrange avec le fait que c'est des timesteps de 6h et pas de jours à voir
-    hours_of_day = hour_of_day % 24
+    day_in_years = len(t) / 24  # 365 or 366
+    hours_of_day = t % 24
+    day_of_years = t // 24
     return torch.stack(
         (
             torch.sin(2 * torch.pi * hours_of_day),  # sin temporal embedding